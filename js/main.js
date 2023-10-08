ab716d14d1f9f7453360802418e4bf41d58b2996
arti876 <e.artem.v@gmail.com>
2023-10-08 12:25:38 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+// burger-menu
+
+const hamb = document.querySelector("#hamb");
+const popup = document.querySelector("#popup");
+const body = document.body;
+
+// Клонируем меню, чтобы задать свои стили для мобильной версии
+const menu = document.querySelector("#menu").cloneNode(1);
+
+// При клике на иконку hamb вызываем ф-ию hambHandler
+hamb.addEventListener("click", hambHandler);
+
+// Выполняем действия при клике ..
+function hambHandler(e) {
+  e.preventDefault();
+  // Переключаем стили элементов при клике
+  popup.classList.toggle("open");
+  hamb.classList.toggle("active");
+  body.classList.toggle("noscroll");
+  renderPopup();
+}
+
+// Здесь мы рендерим элементы в наш попап
+function renderPopup() {
+  popup.appendChild(menu);
+}
+
+// Код для закрытия меню при нажатии на ссылку
+const links = Array.from(menu.children);
+
+// Для каждого элемента меню при клике вызываем ф-ию
+links.forEach((link) => {
+  link.addEventListener("click", closeOnClick);
+});
+
+// Закрытие попапа при клике на меню
+function closeOnClick() {
+  popup.classList.remove("open");
+  hamb.classList.remove("active");
+  body.classList.remove("noscroll");
+}
+
 // header-scroll
 
 $(function () {
@@ -41,14 +83,14 @@ let mySwiperCustom = new Swiper('.swiper-custom', {
 	// автовысота слайдов
 	autoHeight: true,
 	// автопрокрутка
-	// autoplay: {
+	autoplay: {
 		// пауза между прокруткой
-		// delay: 3000,
+		delay: 3000,
 		// отключить после ручного переключения
-		// disableOnInteraction: true,
+		disableOnInteraction: true,
 		// закончить на последнем слайде
-		// stopOnLastSlide: true,
-	// }
+		stopOnLastSlide: true,
+	}
 });
 
 let myPaginationCustom = new Swiper('.pagination-custom', {

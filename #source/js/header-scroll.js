c1888ea167591cd84a4f8eacb4384514e935407d
arti876 <e.artem.v@gmail.com>
2023-10-09 22:41:10 +0300

    commit

--- #source/js/header-scroll.js
+++ #source/js/header-scroll.js
@@ -3,7 +3,7 @@
 $(function () {
 	$(document).scroll(function () {
 		var $header = $("header");
-		var $header__container = $(".navbar");
+		var $header__container = $(".header__wrapper");
 		$header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
 		$header__container.toggleClass('scrolled-border', $(this).scrollTop() > $header.height());
 	});
--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ function closeOnClick() {
 $(function () {
 	$(document).scroll(function () {
 		var $header = $("header");
-		var $header__container = $(".navbar");
+		var $header__container = $(".header__wrapper");
 		$header.toggleClass('scrolled', $(this).scrollTop() > $header.height());
 		$header__container.toggleClass('scrolled-border', $(this).scrollTop() > $header.height());
 	});
